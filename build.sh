9c206072fe16f6f85276bef7667e59930e6af2aa
Álvaro Castro-Castilla <alvatar@protonmail.com>
2022-10-05 17:29:30 +0300

    Ensure build script runs with the proper environment

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
+cp shell.nix uniswap-interface/
 cd uniswap-interface/
-yarn build
+nix-shell shell.nix --command 'yarn build'
 cd ..
 mv uniswap-interface/build uniswap-glob
 cd uniswap-glob
-find . -name "*.map" | xargs rm
