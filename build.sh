e73ea848b2fd24d399f9f68544202c1929009ec9
Álvaro Castro-Castilla <alvatar@protonmail.com>
2022-10-21 13:32:09 +0300

    Update interface

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 cp shell.nix uniswap-interface/
 cd uniswap-interface/
+nix-shell shell.nix --command 'yarn'
 nix-shell shell.nix --command 'yarn build'
 cd ..
 mv uniswap-interface/build uniswap-glob
 cd uniswap-glob
+find . -iname "*.map" | xargs rm
